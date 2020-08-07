2f98c3c7a4acbc69413c82fe3f34c8722c004962
José Hernández <jhernanq@everis.com>
2020-08-07 11:34:18 -0400

    avance nav bar

--- projectadm/src/app/app-routing.module.ts
+++ projectadm/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'login', loadChildren: () => import('./auth/login/login.module').then(m => m.LoginModule) },
   { path: 'register', loadChildren: () => import('./auth/register/register.module').then(m => m.RegisterModule) }
