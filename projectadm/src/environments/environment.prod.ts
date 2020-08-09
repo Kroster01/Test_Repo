83bf8faae74179785791f92193d1419b8e8e757b
José Hernández <jhernanq@everis.com>
2020-08-08 22:45:16 -0400

    actualización generidade archivos

--- projectadm/src/environments/environment.prod.ts
+++ projectadm/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCnQmwu6CpmKBzhHboPHWlOo3bCntHDmr8',
+    authDomain: 'ng-login-92a0a.firebaseapp.com',
+    databaseURL: 'https://ng-login-92a0a.firebaseio.com',
+    projectId: 'ng-login-92a0a',
+    storageBucket: 'ng-login-92a0a.appspot.com',
+    messagingSenderId: '58944207007',
+    appId: '1:58944207007:web:1216585452fb9ecd200aaa'
+  }
 };
