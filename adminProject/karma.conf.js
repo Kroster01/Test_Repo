05e02be63f569b669ee5b9c87e005a089d25d4dd
José Hernández <jhernanq@everis.com>
2020-08-23 15:31:16 -0400

    Project Base

--- adminProject/karma.conf.js
+++ adminProject/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/projectadm'),
+      dir: require('path').join(__dirname, './coverage/adminProject'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- adminProject/src/environments/environment.ts
+++ adminProject/src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: 'AIzaSyCnQmwu6CpmKBzhHboPHWlOo3bCntHDmr8',
-    authDomain: 'ng-login-92a0a.firebaseapp.com',
-    databaseURL: 'https://ng-login-92a0a.firebaseio.com',
-    projectId: 'ng-login-92a0a',
-    storageBucket: 'ng-login-92a0a.appspot.com',
-    messagingSenderId: '58944207007',
-    appId: '1:58944207007:web:1216585452fb9ecd200aaa'
-  }
+  production: false
 };
 
 /*
