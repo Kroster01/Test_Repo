246e6b191ef2d173be74a30979d1898733b845dd
José Hernández Q <jhernanq@everis.com>
2020-10-13 04:52:50 -0300

    Quinto sección de llamadas a servicios
    solo el admin puede realizar las operaciones

--- generic_project/src/routes/user.ts
+++ generic_project/src/routes/user.ts
@@ -1,22 +1,23 @@
 import { checkJwt } from "./../middlewares/jwt";
 import { Router } from "express";
 import { UserController } from "../controller/UserController";
+import { checkRole } from '../middlewares/role';
 
 const router = Router();
 
 // Get al users
-router.get('/', [checkJwt], UserController.getAll);
+router.get('/', [checkJwt, checkRole(['admin'])], UserController.getAll);
 
 // Get one User
-router.get('/:id', [checkJwt], UserController.getById);
+router.get('/:id', [checkJwt, checkRole(['admin'])], UserController.getById);
 
-// Create a new User
-router.post('/', [checkJwt], UserController.newUser);
+// Create a new User solo el admin puede crear users
+router.post('/', [checkJwt, checkRole(['admin'])], UserController.newUser);
 
 // Edit User
-router.patch('/:id', [checkJwt], UserController.editUser);
+router.patch('/:id', [checkJwt, checkRole(['admin'])], UserController.editUser);
 
 // Delete User
-router.delete('/:id', [checkJwt], UserController.deleteUser);
+router.delete('/:id', [checkJwt, checkRole(['admin'])], UserController.deleteUser);
 
 export default router;
